59f0587523e9a978cd029764209e6043baefb568
ArrShi <sergeu8900@gamil.com>
2020-07-11 20:00:05 +0300

    Что-то новое

--- app/src/main/java/com/example/notes/EventFragment.kt
+++ app/src/main/java/com/example/notes/EventFragment.kt
@@ -3,12 +3,14 @@ package com.example.notes
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import com.example.database.DBHandler
 import com.prolificinteractive.materialcalendarview.CalendarDay
@@ -29,17 +31,17 @@ class EventFragment:Fragment()
         val tempView=inflater.inflate(R.layout.fragment_event, container, false)
         val dbHandler=DBHandler(requireContext())
         val widget:MaterialCalendarView=tempView.findViewById(R.id.eventsCalendar)
-        val listEvents=dbHandler.getAll()
-        val rand= kotlin.random.Random
+        val listEvents=dbHandler.getEvents()
         val calendar=Calendar.getInstance()
        for (elem in listEvents){
             calendar.time=elem.notifDate!!
            val calendarDay=CalendarDay.from(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH)+1,calendar.get(Calendar.DAY_OF_MONTH))
-            widget.addDecorator(CurrentDayDecorator(context = requireActivity(),currentDay = calendarDay,colorId = rand.nextInt(0,2)))
+            widget.addDecorator(CurrentDayDecorator(context = requireActivity(),currentDay = calendarDay))
         }
         widget.setOnDateChangedListener{
                 _, date, selected ->
-            
+            calendar.set(date.year,date.month,date.day)
+            val eventsByDay=dbHandler.getEventsByDate(calendar.time)
         }
 
         return tempView
@@ -47,7 +49,7 @@ class EventFragment:Fragment()
 
 }
 
-class CurrentDayDecorator(context: Activity?, currentDay: CalendarDay,colorId:Int) : DayViewDecorator {
+class CurrentDayDecorator(context: Activity?, currentDay: CalendarDay) : DayViewDecorator {
     private val drawable: Drawable?
     var myDay = currentDay
     override fun shouldDecorate(day: CalendarDay): Boolean {
@@ -57,14 +59,8 @@ class CurrentDayDecorator(context: Activity?, currentDay: CalendarDay,colorId:In
     override fun decorate(view: DayViewFacade) {
         view.setSelectionDrawable(drawable!!)
     }
-
     init {
-        when (colorId) {
-            0 -> drawable = ContextCompat.getDrawable(context!!, R.color.daysColorID0)
-            1 -> drawable = ContextCompat.getDrawable(context!!, R.color.daysColorID1)
-            2 -> drawable = ContextCompat.getDrawable(context!!, R.color.daysColorID2)
-            else->drawable = ContextCompat.getDrawable(context!!, R.color.daysColorDefault)
+          drawable=ContextCompat.getDrawable(context!!,R.drawable.ic_outline_radio_button_unchecked_24)
+            DrawableCompat.setTint(drawable!!,Color.GREEN)
         }
-
-    }
 }
